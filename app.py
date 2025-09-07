1123d173980dd935459038b0d47f969bdc89f130
maruthi1225 <maruthi.nayak12345@gmail.com>
2025-09-07 13:00:36 +0530

    app edits

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ def handler(event, context):
     print("InturnStream handler")
     return event
 
-<<<<<<< HEAD
 print("InturnStream loaded")
-=======
 
 print("Git Work Shop")
->>>>>>> 86f5c31ce36058b8e4ae00ba9915af065df22321
+
